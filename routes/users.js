53a49b1d25958f252665863afab2d12b3ea7062b
Muhammad Fari <fari.msenju@gmail.com>
2018-11-23 13:58:50 +0700

    add Login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/login', (req, res) => {
+  res.render('auth/login')
+})
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body
+  models.User.findOne({
+    where: {
+      username: username
+    }
+  }).then(user => {
+    if(user != null){
+      const checkPassword = bcrypt.compareSync(password, user.password); // true
+      if(checkPassword === true){
+        res.redirect('/siswas')
+      }else {
+        res.redirect('/users/login')
+      }
+    }else {
+      res.redirect('/users/login')
+    }
+  })
 });
 
 module.exports = router;
---
+++ seeders/20181123055816-user-seeder.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    const password = bcrypt.hashSync('rahasia', 10);
+    return queryInterface.bulkInsert('Users', [{
+      username: 'doni',
+      password: password
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
